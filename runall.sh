0dafdbedf0e7f7552f1fcb6783639f435352dc9b
ralph yozzo <ralph@brooklynmarathon.com>
2025-01-18 15:21:25 -0500

    Sat Jan 18 03:21:24 PM EST 2025

---
+++ runall.sh
@@ -0,0 +1,7 @@
+#!
+DATE=$(date)
+curl 'https://www.nyccfb.info/public/reports/pclt_25.htm' > nyccfb_info_public_reports_pclt_25.htm
+html2csv < nyccfb_info_public_reports_pclt_25.htm >nyccfb_info_public_reports_pclt_25.htm.csv
+git commit -m "${DATE}" .
+git push
+
