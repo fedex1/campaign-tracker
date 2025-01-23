f42baa3ff689e96186b2990bf3f2aecd0e2792a9
ralph yozzo <ralph@brooklynmarathon.com>
2025-01-23 11:01:51 -0500

    Thu Jan 23 11:01:50 AM EST 2025

--- runall.sh
+++ runall.sh
@@ -3,5 +3,6 @@ DATE=$(date)
 curl 'https://www.nyccfb.info/public/reports/pclt_25.htm' > nyccfb_info_public_reports_pclt_25.htm
 html2csv < nyccfb_info_public_reports_pclt_25.htm >nyccfb_info_public_reports_pclt_25.htm.csv
 git commit -m "${DATE}" .
+git pull --quiet
 git push
 
