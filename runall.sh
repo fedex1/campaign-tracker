0002342c070c566a0cfd5e99f1d666b025daefa6
ralph yozzo <ralph@brooklynmarathon.com>
2025-01-25 10:12:26 -0500

    ralph-desktop

--- runall.sh
+++ runall.sh
@@ -6,3 +6,4 @@ git commit -m "${DATE}" .
 git pull --quiet
 git push
 
+# git diff --unified=0  f42baa3ff689e96186b2990bf3f2aecd0e2792a9  -- nyccfb_info_public_reports_pclt_25.htm.csv > new.modified.campaigns.txt
