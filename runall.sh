96efeddffcd6530bcc43c20d2b5eb0934cc94627
ralph yozzo <ralph@brooklynmarathon.com>
2025-01-30 18:43:52 -0500

    ralph-desktop

--- runall.sh
+++ runall.sh
@@ -1,9 +1,13 @@
 #!
 DATE=$(date)
-curl 'https://www.nyccfb.info/public/reports/pclt_25.htm' > nyccfb_info_public_reports_pclt_25.htm
-html2csv < nyccfb_info_public_reports_pclt_25.htm >nyccfb_info_public_reports_pclt_25.htm.csv
-git commit -m "${DATE}" .
-git pull --quiet
-git push
+curl 'https://www.nyccfb.info/public/reports/pclt_25.htm' >nyccfb_info_public_reports_pclt_25.htm
+OUT="nyccfb_info_public_reports_pclt_25.htm.csv"
+
+html2csv <nyccfb_info_public_reports_pclt_25.htm >$OUT
+if [ -s "$OUT" ]; then
+	git commit -m "${DATE}" .
+	git pull --quiet
+	git push
+fi
 
 # git diff --unified=0  f42baa3ff689e96186b2990bf3f2aecd0e2792a9  -- nyccfb_info_public_reports_pclt_25.htm.csv > new.modified.campaigns.txt
