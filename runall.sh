dd26ba1f8c1b85478a3ca66c806e45a80e17e196
ralph yozzo <ralph@brooklynmarathon.com>
2025-02-20 17:20:46 -0500

    ralph-desktop

--- runall.sh
+++ runall.sh
@@ -6,7 +6,7 @@ OUTHTML="changes.2025.html"
 
 html2csv <nyccfb_info_public_reports_pclt_25.htm >$OUT
 if [ -s "$OUT" ]; then
-echo '<head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></head><body>' >$OUTHTML
+echo '<head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></head><body><pre>' >$OUTHTML
     source ~/.nvm/nvm.sh
     nvm use v18.18.2
     # git log --patch --color-words  -- $OUT|head -2000| npx ansi-to-html >> $OUTHTML
