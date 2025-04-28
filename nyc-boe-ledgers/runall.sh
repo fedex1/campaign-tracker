a9c8c9abad5a586a61ca08146c7a09457b861096
ralph yozzo <ralph@brooklynmarathon.com>
2025-04-28 17:45:38 -0400

    ralph-desktop

--- nyc-boe-ledgers/runall.sh
+++ nyc-boe-ledgers/runall.sh
@@ -3,6 +3,7 @@ DATE=$(date)
 wget -r -nv  https://static.repmyblock.org/BOEDocs/20250624/
 
 find . -name "*.pdf" -exec pdftotext -layout {} \;
+git add .
 
 	git commit -m "${DATE}" .
 	git pull --quiet
